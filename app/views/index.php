faae39af22d1df5214edd9e59979707de2ffdb50
Alejandro M <alejandro@Ales-MacBook-Air.local>
2014-01-29 00:09:17 -0300

    Theme

--- app/views/index.php
+++ app/views/index.php
@@ -1,10 +1,9 @@
 <html>
 <head>
 	<title>Laravel RequireJS + Backbone.js boilerplate</title>
+	<link rel="stylesheet" type="text/css" href="/css/libs/bootstrap.css" />
 </head>
 <body>
-
-
 	<script type="text/javascript" src="js/libs/require.js" data-main="js/main"></script>
 </body>
 </html>
